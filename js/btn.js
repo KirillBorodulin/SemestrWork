de0e5d9526f07fb4919ce8fd4b1ac4eef6f31ed9
Студенческая <@rbiu.local>
2025-06-03 11:10:30 +0500

    new

--- js/btn.js
+++ js/btn.js
@@ -1,8 +1,8 @@
-const btn = document.getElementById("navbar-toggler");//выбрать элемент кнопку
-const menus = document.querySelectorAll(".menu");//выбрать все меню
-btn.addEventListener('click', () => {//когда пользователь кликнет по кнопке
-    for (let el of menus) {//перебрать все меню и к каждому
-        el.classList.toggle("show");//применить убрать стиль show
+const btn = document.getElementById("navbar-toggler");
+const menus = document.querySelectorAll(".menu");
+btn.addEventListener('click', () => {
+    for (let el of menus) {
+        el.classList.toggle("show");
     }
 })
 
